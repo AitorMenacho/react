2929563b71b67bc21d7599d9843c9a2847d69030
Aitor Menacho <menacho.aitor@gmail.com>
2022-09-26 22:14:38 +0200

    Update index.js

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,5 +1,23 @@
 
 
+//Variables y constantes
 
-console.log('Hola Mundo');
+const nombre   = 'Aitor';
+const apellido = 'Menacho';
 
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+//var NO se debe usar...
+if ( true ) {
+    const nombre = 'Peter';
+
+    console.log(nombre);
+}
+
+console.log( valorDado );
