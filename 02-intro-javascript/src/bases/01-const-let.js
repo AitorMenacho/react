78a43b9d4124cd0f47b79bac39a4223d6e83b83b
Aitor Menacho <menacho.aitor@gmail.com>
2022-09-26 22:15:31 +0200

    Update

---
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,23 @@
+
+
+//Variables y constantes
+
+const nombre   = 'Aitor';
+const apellido = 'Menacho';
+
+let valorDado = 5;
+
+console.log(nombre, apellido, valorDado);
+
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado);
+
+//var NO se debe usar...
+if ( true ) {
+    const nombre = 'Peter';
+
+    console.log(nombre);
+}
+
+console.log( valorDado );
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,23 +1,3 @@
 
 
-//Variables y constantes
 
-const nombre   = 'Aitor';
-const apellido = 'Menacho';
-
-let valorDado = 5;
-
-console.log(nombre, apellido, valorDado);
-
-valorDado = 4;
-
-console.log(nombre, apellido, valorDado);
-
-//var NO se debe usar...
-if ( true ) {
-    const nombre = 'Peter';
-
-    console.log(nombre);
-}
-
-console.log( valorDado );
