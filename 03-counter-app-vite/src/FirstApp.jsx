5dad0c0d871b4404613ca4cf66e9ee5908037a72
Aitor Menacho <menacho.aitor@gmail.com>
2022-09-30 19:40:57 +0200

    add styles

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,9 +1,13 @@
 // import { Fragment } from "react"; ----> No es necesario hoy en día, pero es útil.
 
-export const FirstApp = () => {
+
+
+export const FirstApp = () => {  
+
   return (
     <>
       <h1>Aitor</h1>
+      {/* <code>{ JSON.stringify(newMessage) }</code> */}
       <p>Soy un subtitulo</p>
     </>
   )
