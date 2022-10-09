c2e13500693e8165f6ab49e197f53a38d2654eb9
Aitor Menacho <menacho.aitor@gmail.com>
2022-10-09 16:52:07 +0200

    función que suma 1

---
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -0,0 +1,44 @@
+
+
+import { useState } from 'react';
+//Importamos los PropTypes para que funcione todo
+import { PropTypes } from "prop-types";
+
+//Exportamos CounterApp, todo lo que esté dentro del return será mostrado o devuelto al cliente.
+//Para usar algo devuelto del componente padre hay que desestructurarlo.
+export const CounterApp = ({ value }) => {
+
+    const [ counter, setCounter ] = useState( 0 )
+
+    const handleAdd = () => {
+        
+        setCounter( counter + 1 )
+
+    }
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2> { counter } </h2>
+
+            <button onClick={ handleAdd } >
+                +1
+            </button>
+
+        </>
+
+    )
+
+}
+
+//Asignamos de que tipo es cada variable y si es o no obligatoria.
+CounterApp.propTypes = {
+    value : PropTypes.number
+}
+
+//Asignamos valor por defecto.
+CounterApp.defaultProps = {
+
+    value : 'Sin valor asignado'
+
+}
