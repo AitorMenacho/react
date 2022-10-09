7db033dd45c06c550b27753dce54f340623b7a60
Aitor Menacho <menacho.aitor@gmail.com>
2022-10-09 17:36:36 +0200

    inicio jest

---
+++ 03-counter-app-vite/tests/demo.test.js
@@ -0,0 +1,20 @@
+
+describe( 'Pruebas en <DemoComponent />', () => {
+
+    test('Este prueba no debe de fallar', () => { 
+        
+        //1. Inicializacion
+    
+        const message1 = 'Hola mundo'
+    
+        //2. Estímulo
+        const message2 = message1.trim()
+    
+    
+        //3. Observar el comportamiento.... esperado
+        expect( message1 ).toBe( message2 )
+    
+    
+     })
+
+})
