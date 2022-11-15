4f699ef8e9d93c3a1f062a7b7d391b08fb1e5d09
Aitor Menacho <menacho.aitor@gmail.com>
2022-11-15 08:35:14 +0100

    hooks

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HooksApp } from './HooksApp'
+import { Padre } from './07-tarea-memo/Padre'
+// import { CallBackHook } from './06-memos/CallBackHook'
+// import { MemoHook } from './06-memos/MemoHook'
+// import { Memorize } from './06-memos/Memorize'
+// import { FocusScreen } from './04-useRef/FocusScreen'
+// import { Layout } from './05-useLoayoutEffect/Layout'
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
+// import { CounterApp } from './01-useState/CounterApp'
+// import { HooksApp } from './HooksApp'
+
+import './08-useReducer/intro-reducer'
 
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HooksApp />
-  </React.StrictMode>
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <Padre />
+// )
