516edf6bc7bfbbba37cca7db36d449cc6ab7fb7a
Aitor Menacho <menacho.aitor@gmail.com>
2022-12-20 09:16:52 +0100

    Router

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 // import { Padre } from './07-tarea-memo/Padre'
-import { TodoApp } from "./08-useReducer/TodoApp";
+// import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 // import { CallBackHook } from './06-memos/CallBackHook'
 // import { MemoHook } from './06-memos/MemoHook'
 // import { Memorize } from './06-memos/Memorize'
@@ -18,4 +20,8 @@ import { TodoApp } from "./08-useReducer/TodoApp";
 
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<TodoApp />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <MainApp />
+  </BrowserRouter>
+);
