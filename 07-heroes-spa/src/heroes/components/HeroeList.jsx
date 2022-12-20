10e1c53d56ca40da3c2bfda52d6dfc0a03dad184
Aitor Menacho <menacho.aitor@gmail.com>
2022-12-20 21:13:49 +0100

    add heroes app

---
+++ 07-heroes-spa/src/heroes/components/HeroeList.jsx
@@ -0,0 +1,14 @@
+import { HeroCard } from "./";
+import { getHeroesByPublisher } from "../helpers";
+
+export const HeroeList = ({ publisher }) => {
+  const heroes = getHeroesByPublisher(publisher);
+
+  return (
+    <div className="row rows-cols-1 row-cols-md-3 g-3" >
+      {heroes.map((heroe) => (
+        <HeroCard key={ heroe.id } { ...heroe } />
+      ))}
+    </div>
+  );
+};
