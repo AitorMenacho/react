490188102765818b6c7311d545c591b091b26ccb
Aitor Menacho <menacho.aitor@gmail.com>
2022-12-22 13:08:33 +0100

    App of Superhero

--- 07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-const charactersByHero = ({ alter_ego, characters }) => {
+const CharactersByHero = ({ alter_ego, characters }) => {
 
-  if (alter_ego === characters) return (<></>);
-
-  return <p>{characters}</p>;
+  return ( alter_ego === characters )
+    ? <></>
+    : <p>{characters}</p>
 
 };
 
@@ -21,7 +21,7 @@ export const HeroCard = ({
   //   const charactersByHero = (<p>{characters}</p>)
 
   return (
-    <div className="col">
+    <div className="col animate__animated animate__fadeIn">
       <div className="card">
         <div className="row no-gutters">
           <div className="col-4">
@@ -32,7 +32,7 @@ export const HeroCard = ({
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
 
-              <charactersByHero alter_ego={ alter_ego } characters={ characters } />
+              <CharactersByHero alter_ego={ alter_ego } characters={ characters } />
 
               <p className="card-text">
                 <small className="text-muted">{first_appearance}</small>
--- 07-heroes-spa/src/heroes/helpers/index.js
+++ 07-heroes-spa/src/heroes/helpers/index.js
@@ -1,1 +1,4 @@
+
+export * from './getHeroById';
+export * from './getHeroesByName';
 export * from './getHeroesByPublisher';
