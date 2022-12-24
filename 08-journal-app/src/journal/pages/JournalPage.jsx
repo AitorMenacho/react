442b8f5ba215adb0c78a9a4e34bc552dcd01d5d9
Aitor Menacho <menacho.aitor@gmail.com>
2022-12-24 12:18:18 +0100

    Journal terminado

--- 08-journal-app/src/journal/pages/JournalPage.jsx
+++ 08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,35 @@
-import { Typography } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView, NoteView } from "../views";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant='h1'>JournalPage</Typography>
-    </>
-  )
-}
+    <JournalLayout>
+      {/* <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit voluptas
+        sunt obcaecati quae odit? Numquam sunt facilis iusto explicabo
+        voluptatibus molestiae amet. Nesciunt maxime quibusdam doloremque
+        necessitatibus, magnam aliquam doloribus.
+      </Typography> */}
+
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: "error.main",
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: "fixed",
+          right: 50,
+          bottom: 50,
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
+    </JournalLayout>
+  );
+};
